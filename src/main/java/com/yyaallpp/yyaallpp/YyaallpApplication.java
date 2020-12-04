757c4ecdb6e95511987ac805a75b5b6d5b47013f
yyaallpp <heejoyou@naver.com>
2020-12-04 22:17:24 +0900

    ddd

---
+++ src/main/java/com/yyaallpp/yyaallpp/YyaallpApplication.java
@@ -0,0 +1,13 @@
+package com.yyaallpp.yyaallpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyaallpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YyaallpApplication.class, args);
+	}
+
+}
